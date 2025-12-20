d70cf8d8889a3823d7517bf6836a372da17d00cc
Igor F <igornvf@gmail.com>
2025-12-20 12:38:02 -0300

    minimo.py criado usando algoritmo de regressão linear

---
+++ minimo.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv("diabetes.xls")
+
+print(df.head())
+print(df.columns)
+
+x = df.drop(columns=["Outcome"])
+y = df["Outcome"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=42
+)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_train)
+print("Acurácia:", acc)
+
